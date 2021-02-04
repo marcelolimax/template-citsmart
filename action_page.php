e232f0b482db5ce67672819b9e0897b9390d3dc9
Marcelo <marcelo.suporte.redes@gmail.com>
2021-02-04 09:50:38 -0300

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,301 @@
+<html xmlns:v="urn:schemas-microsoft-com:vml"
+xmlns:o="urn:schemas-microsoft-com:office:office"
+xmlns:w="urn:schemas-microsoft-com:office:word"
+xmlns:m="http://schemas.microsoft.com/office/2004/12/omml"
+xmlns="http://www.w3.org/TR/REC-html40">
+
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=windows-1252">
+<meta name=ProgId content=Word.Document>
+<meta name=Generator content="Microsoft Word 15">
+<meta name=Originator content="Microsoft Word 15">
+<style>
+<!--
+ /* Font Definitions */
+ @font-face
+	{font-family:Helvetica;
+	panose-1:2 11 6 4 2 2 2 2 2 4;
+	mso-font-charset:0;
+	mso-generic-font-family:swiss;
+	mso-font-pitch:variable;
+	mso-font-signature:-536858881 -1073711013 9 0 511 0;}
+@font-face
+	{font-family:"Cambria Math";
+	panose-1:2 4 5 3 5 4 6 3 2 4;
+	mso-font-charset:1;
+	mso-generic-font-family:roman;
+	mso-font-format:other;
+	mso-font-pitch:variable;
+	mso-font-signature:0 0 0 0 0 0;}
+@font-face
+	{font-family:Calibri;
+	panose-1:2 15 5 2 2 2 4 3 2 4;
+	mso-font-charset:0;
+	mso-generic-font-family:swiss;
+	mso-font-pitch:variable;
+	mso-font-signature:-469750017 -1073732485 9 0 511 0;}
+@font-face
+	{font-family:"Bookman Old Style";
+	panose-1:2 5 6 4 5 5 5 2 2 4;
+	mso-font-charset:0;
+	mso-generic-font-family:roman;
+	mso-font-pitch:variable;
+	mso-font-signature:647 0 0 0 159 0;}
+@font-face
+	{font-family:"Segoe UI Symbol";
+	panose-1:2 11 5 2 4 2 4 2 2 3;
+	mso-font-charset:0;
+	mso-generic-font-family:swiss;
+	mso-font-pitch:variable;
+	mso-font-signature:-2147483165 302055407 262144 0 1 0;}
+ /* Style Definitions */
+ p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{mso-style-unhide:no;
+	mso-style-qformat:yes;
+	mso-style-parent:"";
+	margin-top:0cm;
+	margin-right:0cm;
+	margin-bottom:8.0pt;
+	margin-left:0cm;
+	line-height:107%;
+	mso-pagination:widow-orphan;
+	font-size:11.0pt;
+	font-family:"Calibri",sans-serif;
+	mso-ascii-font-family:Calibri;
+	mso-ascii-theme-font:minor-latin;
+	mso-fareast-font-family:Calibri;
+	mso-fareast-theme-font:minor-latin;
+	mso-hansi-font-family:Calibri;
+	mso-hansi-theme-font:minor-latin;
+	mso-bidi-font-family:"Times New Roman";
+	mso-bidi-theme-font:minor-bidi;
+	mso-fareast-language:EN-US;}
+p
+	{mso-style-noshow:yes;
+	mso-style-priority:99;
+	mso-margin-top-alt:auto;
+	margin-right:0cm;
+	mso-margin-bottom-alt:auto;
+	margin-left:0cm;
+	mso-pagination:widow-orphan;
+	font-size:12.0pt;
+	font-family:"Times New Roman",serif;
+	mso-fareast-font-family:"Times New Roman";}
+span.SpellE
+	{mso-style-name:"";
+	mso-spl-e:yes;}
+span.GramE
+	{mso-style-name:"";
+	mso-gram-e:yes;}
+.MsoChpDefault
+	{mso-style-type:export-only;
+	mso-default-props:yes;
+	font-family:"Calibri",sans-serif;
+	mso-ascii-font-family:Calibri;
+	mso-ascii-theme-font:minor-latin;
+	mso-fareast-font-family:Calibri;
+	mso-fareast-theme-font:minor-latin;
+	mso-hansi-font-family:Calibri;
+	mso-hansi-theme-font:minor-latin;
+	mso-bidi-font-family:"Times New Roman";
+	mso-bidi-theme-font:minor-bidi;
+	mso-fareast-language:EN-US;}
+.MsoPapDefault
+	{mso-style-type:export-only;
+	margin-bottom:8.0pt;
+	line-height:107%;}
+@page WordSection1
+	{size:595.3pt 841.9pt;
+	margin:70.85pt 3.0cm 70.85pt 3.0cm;
+	mso-header-margin:35.4pt;
+	mso-footer-margin:35.4pt;
+	mso-paper-source:0;}
+div.WordSection1
+	{page:WordSection1;}
+-->
+</style>
+</head>
+
+<body lang=PT-BR style='tab-interval:35.4pt'>
+
+<div class=WordSection1>
+
+<table class=MsoNormalTable border=0 cellspacing=0 cellpadding=0 width=0
+ style='width:942.25pt;background:white;border-collapse:collapse;mso-yfti-tbllook:
+ 1184'>
+ <tr style='mso-yfti-irow:0;mso-yfti-firstrow:yes;height:40pt'>
+  <td width=1256 colspan=2 style='width:942.25pt;border:solid windowtext 1.0pt;
+  padding:0cm 5.4pt 0cm 5.4pt'>
+  <p class=MsoNormal align=center style='margin-bottom:0cm;margin-bottom:.0001pt;
+  text-align:center;mso-line-height-alt:11.55pt'><b><span style='font-size:
+  18.0pt;font-family:"Helvetica",sans-serif;mso-fareast-font-family:"Times New Roman";
+  color:#0DB0BF;mso-fareast-language:PT-BR'>Central IT - Registro de
+  Atendimento</span></b><span style='mso-ascii-font-family:Calibri;mso-fareast-font-family:
+  "Times New Roman";mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;
+  color:#201F1E;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  </td>
+ </tr>
+ <tr style='mso-yfti-irow:1;height:119.6pt'>
+  <td width=220 style='width:161.55pt;border:solid windowtext 1.0pt;border-top:
+  none;padding:0cm 5.4pt 0cm 5.4pt;height:119.6pt'>
+  <p class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+  11.55pt'><b><span style='font-family:"Bookman Old Style",serif;mso-fareast-font-family:
+  "Times New Roman";mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:
+  PT-BR'>Ordem de Serviço</span></b><span style='mso-ascii-font-family:Calibri;
+  mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  </td>
+  <td width=1036 style='width:780.7pt;border-top:none;border-left:none;
+  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  padding:0cm 5.4pt 0cm 5.4pt;height:119.6pt'>
+  <input type="checkbox" id="RDC" name="RDC" value="RDC"<?php if(isset($_POST['RDC'])) {echo checked} ?>  ><label for="RDC">RDC - ADMINISTRAÇÃO DE REDE DO DATA CENTER</label><br>
+  <input type="checkbox" id="RL" name="RL" value="RL"<?php if(isset($_POST['RL'])) {echo checked} ?>  ><label for="RL">RL - ADMINISTRAÇÃO DE REDE LOCAL ATI</label><br>
+  <input type="checkbox" id="SI" name="SI" value="SI"<?php if(isset($_POST['SI'])) {echo checked} ?>  ><label for="SI">SI – ADMINISTRAÇÃO DE SOLUÇÃO DE SEGURANÇA DA INFORMAÇÃO</label><br>
+  <input type="checkbox" id="SOC" name="SOC" value="SOC"<?php if(isset($_POST['SOC'])) {echo checked} ?>  ><label for="SOC">SOC - SUPORTE E ADMINISTRAÇÃO EM SISTEMAS OPERACIONAIS - CRÍTICOS</label><br>
+  <input type="checkbox" id="SONC" name="SONC" value="SONC"<?php if(isset($_POST['SONC'])) {echo checked} ?>  ><label for="SONC">SONC - SUPORTE E ADMINISTRAÇÃO EM SISTEMAS OPERACIONAIS – NÃO CRÍTICOS</label><br>
+  <input type="checkbox" id="ZBX" name="ZBX" value="ZBX"<?php if(isset($_POST['ZBX'])) {echo checked} ?>  ><label for="ZBX">ZBX - ADMINISTRAÇÃO DE SOLUÇÃO DE MONITORAMENTO – ZABBIX</label><br>
+  <input type="checkbox" id="BDC" name="BDC" value="BDC"<?php if(isset($_POST['BDC'])) {echo checked} ?>  ><label for="BDC">BDC - ADMINISTRAÇÃO DE SISTEMAS DE GERENCIAMENTO DE BANCO DE DADOS - CRÍTICOS</label><br>
+  <input type="checkbox" id="BDNC" name="BDNC" value="BDNC"<?php if(isset($_POST['BDNC'])) {echo checked} ?>  ><label for="BDNC">BDNC - ADMINISTRAÇÃO DE SISTEMAS DE GERENCIAMENTO DE BANCO DE DADOS – NÃO CRÍTICOS</label><br>
+  <input type="checkbox" id="VM" name="VM" value="VM"<?php if(isset($_POST['VM'])) {echo checked} ?>  ><label for="VM">VM - ADMINISTRAÇÃO DE MÁQUINAS VIRTUAIS E VCENTER</label><br>
+  <input type="checkbox" id="HOST" name="HOST" value="HOST"<?php if(isset($_POST['HOST'])) {echo checked} ?>  ><label for="HOST">HOST - ADMINISTRAÇÃO DE HOSTS-HYPERVISOR</label><br>
+  <input type="checkbox" id="ST" name="ST" value="ST"<?php if(isset($_POST['ST'])) {echo checked} ?>  ><label for="ST">ST - ADMINISTRAÇÃO DE INFRAESTRUTURA DE ARMAZENAMENTO (STORAGE</label><br>
+  <input type="checkbox" id="VPX" name="VPX" value="VPX"<?php if(isset($_POST['VPX'])) {echo checked} ?>  ><label for="VPX">VPX - ADMINISTRAÇÃO INFRAESTRUTURA DE ARMAZENAMENTO (STORAGE - VPLEX)</label><br>
+  <input type="checkbox" id="SAN" name="SAN" value="SAN"<?php if(isset($_POST['SAN'])) {echo checked} ?>  ><label for="SAN">SAN - ADMINISTRAÇÃO INFRAESTRUTURA DE ARMAZENAMENTO E CONECTIVIDADE DE DADOS (STORAGE E REDE SAN)</label><br>
+  <input type="checkbox" id="DNS" name="DNS" value="DNS"<?php if(isset($_POST['DNS'])) {echo checked} ?>  ><label for="DNS">DNS - ADMINISTRAÇÃO DA ZONA PE.GOV.BR, DNS, BIND E DJBDNS</label><br>
+  <input type="checkbox" id="EMAIL" name="EMAIL" value="EMAIL"<?php if(isset($_POST['EMAIL'])) {echo checked} ?>  ><label for="EMAIL">EMAIL - ADMINISTRAÇÃO DE SOLUÇÃO DE E-MAIL CORPORATIVO</label><br>
+  <input type="checkbox" id="DRIVE" name="DRIVE" value="DRIVE"<?php if(isset($_POST['DRIVE'])) {echo checked} ?>  ><label for="DRIVE">DRIVE - ADMINISTRAÇÃO DE SOLUÇÃO DE DRIVE CORPORATIVO – ONWCLOUD</label><br>
+ </tr>
+ <tr style='mso-yfti-irow:2;height:51.6pt'>
+  <td width=220 style='width:161.55pt;border:solid windowtext 1.0pt;border-top:
+  none;padding:0cm 5.4pt 0cm 5.4pt;height:51.6pt'>
+  <p class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+  11.55pt'><b><span style='font-family:"Bookman Old Style",serif;mso-fareast-font-family:
+  "Times New Roman";mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:
+  PT-BR'>Descrição</span></b><span style='mso-ascii-font-family:Calibri;
+  mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  </td>
+  <td width=1036 style='width:780.7pt;border-top:none;border-left:none;
+  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  padding:0cm 5.4pt 0cm 5.4pt;height:51.6pt'>
+  <textarea id="descricao" name="descricao" rows="4" cols="50"><?php echo $_POST['descricao']; ?></textarea>
+  </td>
+ </tr>
+ <tr style='mso-yfti-irow:3'>
+  <td width=220 style='width:161.55pt;border:solid windowtext 1.0pt;border-top:
+  none;padding:0cm 5.4pt 0cm 5.4pt'>
+  <p class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+  11.55pt'><b><span style='font-family:"Bookman Old Style",serif;mso-fareast-font-family:
+  "Times New Roman";mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:
+  PT-BR'>Origem</span></b><span style='mso-ascii-font-family:Calibri;
+  mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  </td>
+  <td width=1036 style='width:780.7pt;border-top:none;border-left:none;
+  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  padding:0cm 5.4pt 0cm 5.4pt'>
+  
+  <input type="checkbox" id="ticket" name="ticket" value="ticket" <?php if(isset($_POST['ticket'])) {echo checked} ?>>
+  <label for="ticket">CSATI - Nº :</label><?php echo $_POST['chamado']; ?><br>
+  <input type="checkbox" id="NOC" name="NOC" value="NOC"<?php if(isset($_POST['NOC'])) {echo checked} ?>>
+  <label for="NOC">NOC - Sobreaviso</label><br>
+  <input type="checkbox" id="bacula" name="bacula" value="bacula"<?php if(isset($_POST['bacula'])) {echo checked} ?>>
+  <label for="bacula">Bacula</label><br>
+  <input type="checkbox" id="Zabbix" name="Zabbix" value="Zabbix"<?php if(isset($_POST['Zabbix'])) {echo checked} ?>>
+  <label for="Zabbix">Zabbix</label><br>
+  <input type="checkbox" id="ard" name="ard" value="ard"<?php if(isset($_POST['ard'])) {echo checked} ?>>
+  <label for="ard">Análise Relatório Diário</label><br>
+  <input type="checkbox" id="ad" name="ad" value="ad"<?php if(isset($_POST['ad'])) {echo checked} ?>>
+  <label for="ad">Análise de Desempenho</label><br>
+  <input type="checkbox" id="as" name="as" value="as"<?php if(isset($_POST['as'])) {echo checked} ?>>
+  <label for="as">Análise de Segurança</label><br>
+  <input type="checkbox" id="outros" name="outros" value="outros"<?php if(isset($_POST['outros'])) {echo checked} ?>>
+  Outros:<?php echo $_POST['outrostext']; ?>
+  
+  
+  </td>
+ </tr>
+ <tr style='mso-yfti-irow:4'>
+  <td width=220 style='width:161.55pt;border:solid windowtext 1.0pt;border-top:
+  none;padding:0cm 5.4pt 0cm 5.4pt'>
+  <p class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+  11.55pt'><b><span style='font-family:"Bookman Old Style",serif;mso-fareast-font-family:
+  "Times New Roman";mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:
+  PT-BR'>Ambiente</span></b><span style='mso-ascii-font-family:Calibri;
+  mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  </td>
+  <td width=1036 style='width:780.7pt;border-top:none;border-left:none;
+  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  padding:0cm 5.4pt 0cm 5.4pt'>
+  
+  <input type="checkbox" id="sc" name="sc" value="sc"<?php if(isset($_POST['sc'])) {echo checked} ?>>
+  <label for="sc"><A HREF=http://200.238.105.172/mediawiki/index.php/Servi%C3%A7os_Criticos_Contratuais>Serviço Crítico</a></label><br>
+
+  </td>
+ </tr>
+ <tr style='mso-yfti-irow:5'>
+  <td width=220 style='width:161.55pt;border:solid windowtext 1.0pt;border-top:
+  none;padding:0cm 5.4pt 0cm 5.4pt'>
+  <p class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+  11.55pt'><b><span style='font-family:"Bookman Old Style",serif;mso-fareast-font-family:
+  "Times New Roman";mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:
+  PT-BR'>IP do ambiente</span></b><span style='mso-ascii-font-family:Calibri;
+  mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  </td>
+  <td width=1036 style='width:780.7pt;border-top:none;border-left:none;
+  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  padding:0cm 5.4pt 0cm 5.4pt'>
+
+  
+  <?php echo $_POST['ip']; ?><br>
+  
+
+  </td>
+ </tr>
+ <tr style='mso-yfti-irow:6'>
+  <td width=220 style='width:161.55pt;border:solid windowtext 1.0pt;border-top:
+  none;padding:0cm 5.4pt 0cm 5.4pt'>
+  <p class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+  11.55pt'><b><span style='font-family:"Bookman Old Style",serif;mso-fareast-font-family:
+  "Times New Roman";mso-bidi-font-family:Calibri;color:black;mso-fareast-language:
+  PT-BR'>Solução/Recomendação</span></b><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  </td>
+  <td width=1036 style='width:780.7pt;border-top:none;border-left:none;
+  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  padding:0cm 5.4pt 0cm 5.4pt'>
+
+  <?php echo $_POST['solucao']; ?>
+  
+  </td>
+ </tr>
+ <tr style='mso-yfti-irow:7;mso-yfti-lastrow:yes'>
+  <td width=220 style='width:161.55pt;border:solid windowtext 1.0pt;border-top:
+  none;padding:0cm 5.4pt 0cm 5.4pt'>
+  <p class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+  11.55pt'><b><span style='font-family:"Bookman Old Style",serif;mso-fareast-font-family:
+  "Times New Roman";mso-bidi-font-family:Calibri;color:#201F1E;mso-fareast-language:
+  PT-BR'>Base de Conhecimento/Erros conhecidos/Procedimento</span></b><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:#201F1E;
+  mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  </td>
+  <td width=1036 style='width:780.7pt;border-top:none;border-left:none;
+  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  padding:0cm 5.4pt 0cm 5.4pt'>
+
+  <input type="checkbox" id="wiki" name="wiki" value="wiki"<?php if(isset($_POST['wiki'])) {echo checked} ?>>
+  <label for="wiki">Disponível na Wiki :</label><?php echo $_POST['wikiurl']; ?><br>
+  </td>
+ </tr>
+</table>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+</div>
+
+</body>
+
+</html>
